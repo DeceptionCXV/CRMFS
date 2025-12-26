224ec0aaa19eb1288061ece19bd5f315318ee1f8
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-26 19:29:45 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './contexts/AuthContext';
 import { ToastProvider } from './contexts/ToastContext';
 import ErrorBoundary from './components/ErrorBoundary';
-import ProtectedRoute from './components/ProtectedRoute';
 import CompactLayout from './components/CompactLayout';
-import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import MemberList from './pages/MemberList';
 import MemberDetail from './pages/MemberDetail';
@@ -32,38 +29,26 @@ function App() {
     <ErrorBoundary>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <AuthProvider>
-            <ToastProvider>
-              <Routes>
-                {/* Public Routes */}
-                <Route path="/login" element={<Login />} />
+          <ToastProvider>
+            <Routes>
+              {/* All Routes - No Auth */}
+              <Route path="/" element={<CompactLayout />}>
+                <Route index element={<Dashboard />} />
+                <Route path="members" element={<MemberList />} />
+                <Route path="members/new" element={<AddMember />} />
+                <Route path="members/:id" element={<MemberDetail />} />
+                <Route path="payments" element={<Payments />} />
+                <Route path="reports" element={<Reports />} />
+                <Route path="/deceased" element={<DeceasedMembers />} />
+                <Route path="/deceased/:id" element={<DeceasedDetail />} />
+                <Route path="registration-success" element={<RegistrationSuccess />} />
+              </Route>
 
-                {/* Protected Routes */}
-                <Route
-                  path="/"
-                  element={
-                    <ProtectedRoute>
-                      <CompactLayout />
-                    </ProtectedRoute>
-                  }
-                >
-                  <Route index element={<Dashboard />} />
-                  <Route path="members" element={<MemberList />} />
-                  <Route path="members/new" element={<AddMember />} />
-                  <Route path="members/:id" element={<MemberDetail />} />
-                  <Route path="payments" element={<Payments />} />
-                  <Route path="reports" element={<Reports />} />
-                  <Route path="/deceased" element={<DeceasedMembers />} />
-                  <Route path="/deceased/:id" element={<DeceasedDetail />} />
-                  <Route path="registration-success" element={<RegistrationSuccess />} />
-                </Route>
-
-                {/* 404 Not Found */}
-                <Route path="/404" element={<NotFound />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </ToastProvider>
-          </AuthProvider>
+              {/* 404 Not Found */}
+              <Route path="/404" element={<NotFound />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </ToastProvider>
         </BrowserRouter>
       </QueryClientProvider>
     </ErrorBoundary>
