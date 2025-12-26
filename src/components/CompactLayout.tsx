0078e22994fcbee2783c04d3622859f13fceedf0
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-26 15:55:10 +0000

    Updated CompactLayout.tsx

---
+++ src/components/CompactLayout.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import CollapsibleSidebar from './CollapsibleSidebar';
+import PoweredByBadge from './PoweredByBadge';
+
+interface CompactLayoutProps {
+  children: ReactNode;
+  showSubNav?: boolean;
+  subNav?: ReactNode;
+}
+
+export default function CompactLayout({ children, showSubNav = false, subNav }: CompactLayoutProps) {
+  return (
+    <div className="min-h-screen bg-gray-50 flex">
+      {/* Main Collapsible Sidebar */}
+      <CollapsibleSidebar />
+
+      {/* Sub-navigation (for member details, etc.) */}
+      {showSubNav && subNav && (
+        <div className="hidden lg:block flex-shrink-0">
+          {subNav}
+        </div>
+      )}
+
+      {/* Main Content Area */}
+      <main className="flex-1 overflow-x-hidden">
+        <div className="max-w-[1920px] mx-auto">
+          {/* Add padding to account for mobile hamburger */}
+          <div className="pt-16 md:pt-0 px-4 md:px-6 lg:px-8 py-6">
+            {children}
+          </div>
+        </div>
+      </main>
+
+      {/* Powered By Badge */}
+      <PoweredByBadge />
+    </div>
+  );
+}
